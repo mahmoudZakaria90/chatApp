69f925d55a878f4c3776142e5b4da7572f65040e
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-07-23 00:42:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const server = app.listen(process.env.port || 3000, function(){
+	console.log('listening!')
+})
+
+app.use(express.static('public'));
+
+const io = socket(server);
+io.on('connection', function(socket){
+	console.log('Socket', socket.id)
+	socket.on('chat', function(data){
+		io.sockets.emit('update', data)
+	})
+	socket.on('typing', function(data){
+		socket.broadcast.emit('updateTyping', data);
+	})
+})
+
---
+++ public/chat.js
@@ -0,0 +1,39 @@
+const socket = io.connect('http://localhost:3000');
+
+//DOM
+const chat = document.querySelector('#chat');
+const chatWin = document.querySelector('#chatWin');
+const chatInner = document.querySelector('#chatInner');
+const chatUser = document.querySelector('#chatUser');
+const chatMsg = document.querySelector('#chatMsg');
+const chatTyping = document.querySelector('#chatTyping');
+
+
+function emitChat(e){
+	e.preventDefault();
+	const user = chatUser.value;
+	const msg = chatMsg.value;
+	socket.emit('chat', {user, msg})
+}
+
+function updateChat(data){
+	chatInner.innerHTML += `<p><strong>${data.user}: </strong> <span>${data.msg}</span></p>`;
+	chatUser.value = '';
+	chatMsg.value = '';
+	chatTyping.innerHTML = '';
+}
+
+function updateTyping(data) {
+	chatTyping.innerHTML = `${data} is typing a message...`
+}
+
+//Front-end listeners
+chat.addEventListener('submit', emitChat);
+chatMsg.addEventListener('keypress', function(){
+	socket.emit('typing', chatUser.value)
+})
+
+
+//Socket listeners
+socket.on('update', updateChat)
+socket.on('updateTyping', updateTyping)

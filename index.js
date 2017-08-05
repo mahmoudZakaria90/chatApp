5672fe765988a6f9299a949d80b9ee759e266343
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-08-05 20:44:07 +0200

    added notification and enhancments

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const socket = require('socket.io');
-
+const os = require('os');
 const app = express();
 
 const server = app.listen(process.env.port || 4000, function(){
-	console.log('listening!')
+	console.log('listening!');
+	console.log(os.hostname())
 })
 
 app.use(express.static('public'));
 
+
 const io = socket(server);
 io.on('connection', function(socket){
 	console.log('Socket', socket.id)
+	io.sockets.emit('clientName', os.hostname().split('s-')[0])
 	socket.on('chat', function(data){
 		io.sockets.emit('update', data)
 	})
 	socket.on('typing', function(data){
 		socket.broadcast.emit('updateTyping', data);
 	})
+	socket.on('sound', function(data){
+		socket.broadcast.emit('soundBack', data)
+	})
 })
+
 
--- public/chat.js
+++ public/chat.js
@@ -4,36 +4,55 @@ const socket = io.connect('http://localhost:4000');
 const chat = document.querySelector('#chat');
 const chatWin = document.querySelector('#chatWin');
 const chatInner = document.querySelector('#chatInner');
-const chatUser = document.querySelector('#chatUser');
 const chatMsg = document.querySelector('#chatMsg');
 const chatTyping = document.querySelector('#chatTyping');
+const client = document.querySelector('#clientName');
+let notif = new Audio('https://raw.githubusercontent.com/mahmoudZakaria90/myCodePenStuff/master/audio/notification.mp3');
+let backUp = [];
+let chatUser;
 
 
 function emitChat(e){
 	e.preventDefault();
-	const user = chatUser.value;
+	const user = chatUser;
 	const msg = chatMsg.value;
-	socket.emit('chat', {user, msg})
+	backUp.push({user, msg});
+	socket.emit('chat', {user, msg});
+	socket.emit('sound', backUp);
 }
 
 function updateChat(data){
 	chatInner.innerHTML += `<p><strong>${data.user}: </strong> <span>${data.msg}</span></p>`;
-	chatUser.value = '';
 	chatMsg.value = '';
 	chatTyping.innerHTML = '';
+	if(chatInner.offsetHeight >= chatWin.offsetHeight) {
+		chatWin.scrollTop = chatInner.offsetHeight
+	}
 }
 
 function updateTyping(data) {
 	chatTyping.innerHTML = `${data} is typing a message...`
 }
 
+function clientName(data) {
+	chatUser = data;
+	client.innerHTML = `Logged in as, <span>${data}</span>`
+}
+
+function notification(data) {
+	document.title = `Messages (${data.length})`;
+	notif.play()
+}
+
 //Front-end listeners
 chat.addEventListener('submit', emitChat);
 chatMsg.addEventListener('keypress', function(){
-	socket.emit('typing', chatUser.value)
+	socket.emit('typing', chatUser)
 })
 
 
 //Socket listeners
 socket.on('update', updateChat)
 socket.on('updateTyping', updateTyping)
+socket.on('clientName', clientName)
+socket.on('soundBack', notification)
